b6d9b8800ec40b299da0221fce080ba43a75ae54
gidenilson <gidenilson@gmail.com>
2021-10-20 15:06:16 -0300

    commit

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,16 +2,14 @@
 
 namespace App\Controllers;
 
-
 class HomeController
 {
-
     public function index()
     {
 
-         header('Content-Type: application/json');
-         echo json_encode($_REQUEST);
-         
+        header('Content-Type: application/json');
+        echo json_encode($_REQUEST);
+
     }
 
 }
