3c988b003c526c21f98112163c9337295f77f3f9
= <gidenilson@gmail.com>
2021-12-21 16:19:21 -0300

    alteração nas tabelas

--- src/Controllers/SurveyController.php
+++ src/Controllers/SurveyController.php
@@ -1,52 +1,31 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Models\Option;
 use App\Models\Survey;
-use Illuminate\Database\Capsule\Manager as DB;
-use App\Validators\Validator;
 
 class SurveyController
 {
-
-    private $validator;
-
-    public function __construct()
-    {
-        $this->validator = new Validator();
-    }
 
     public function index()
     {
         $surveys = Survey::all();
         header('Content-Type: application/json');
-        echo json_encode($surveys);
+         echo json_encode($surveys);
     }
-
-    public function store()
-    {
-        $valid = $this->validator->validate($_REQUEST, [
-            'title' => 'required',
-            'description' => 'required',
-            'multiple' => 'required'
-        ]);
-
-        if (! $valid) {
-            $response = [
-                'message' => $this->validator->message
-            ];
-            header('Content-Type: application/json');
-            echo json_encode($response);
-            exit();
-        }
-
-        $survey = new Survey();
+    public function store(){
+        $survey = new Survey;
         $survey->title = $_REQUEST['title'];
         $survey->description = $_REQUEST['description'];
         $survey->multiple = $_REQUEST['multiple'];
         $survey->save();
-
-        $this->updateOptions($survey, $_REQUEST['option']);
+        
+        foreach($_REQUEST['option'] as $text){
+            $option = new Option;
+            $option->text = $text;
+            $survey->options()->save($option);
+        }
 
         $survey->save();
         $survey->refresh();
@@ -55,32 +34,17 @@ class SurveyController
         echo json_encode($survey);
     }
 
-    public function get($id)
-    {
+    public function get($id){
         $survey = Survey::find($id);
         header('Content-Type: application/json');
         echo json_encode($survey);
     }
 
-    public function update($id)
-    {
+    public function update($id){
         $survey = Survey::find($id);
 
-        if (! $survey) {
-            $response = [
-                'message' => 'not found'
-            ];
-            header('Content-Type: application/json');
-            echo json_encode($response);
-            exit();
-        }
-
-        $this->updateOptions($survey, $_REQUEST['option']);
-
-        foreach ($_REQUEST as $field => $value) {
-            if(isset($survey->$field)){
-                $survey->$field = $value;
-            }            
+        foreach($_REQUEST as $field=>$value){
+            $survey->$field = $value;
         }
 
         $survey->save();
@@ -88,25 +52,16 @@ class SurveyController
         echo json_encode($survey);
     }
 
-    public function delete($id)
-    {
+    public function delete($id){
         $survey = Survey::find($id);
-        if ($survey) {
+        if($survey) {
             $survey->delete();
         }
 
+
         header('Content-Type: application/json');
         echo json_encode($survey);
+
     }
 
-    private function updateOptions($survey, $options)
-    {
-        DB::table('options')->where('survey_id', $survey->id)->delete();
-        
-        foreach ($options as $text) {
-            $option = new Option();
-            $option->text = $text;
-            $survey->options()->save($option);
-        }
-    }
 }
